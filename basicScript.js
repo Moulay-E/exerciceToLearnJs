40a345d2e8cc4783264915b774106702d2d5fbfb
Moulay-E <ayoub.edda2@gmail.com>
2024-04-23 13:34:15 +0200

    advancement on nav menu

--- basicScript.js
+++ basicScript.js
@@ -1,14 +1,14 @@
-function DarkModeSwitch(idContainer){
+function darkModeSwitch(idContainer){
 let colorToChange = document.getElementById(idContainer);
 //a method of DOMTokenList for add a class or remove if present
 document.body.classList.toggle("darkMode");
 }
 
-document.querySelectorAll('.ulParent > li').forEach(function(menuItem) {
-    menuItem.addEventListener('mouseenter', function() {
-        this.querySelector('.hide').classList.remove('hide');
-    });
-    menuItem.addEventListener('mouseleave', function() {
-        this.querySelector('.hide').classList.add('hide');
-    });
-});
+// document.querySelectorAll('.ulParent > li').forEach(function(menuItem) {
+//     menuItem.addEventListener('mouseenter', function() {
+//         this.querySelector('.hide').classList.remove('hide');
+//     });
+//     menuItem.addEventListener('mouseleave', function() {
+//         this.querySelector('.hide').classList.add('hide');
+//     });
+// });
