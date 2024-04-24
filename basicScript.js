663ced81904ef5bd426eac800df7a633601e45fc
Moulay-E <ayoub.edda2@gmail.com>
2024-04-24 09:16:15 +0200

    clean css, add code reveal to page, possibility to hidden element on page

--- basicScript.js
+++ basicScript.js
@@ -1,7 +1,25 @@
-function darkModeSwitch(idContainer){
-let colorToChange = document.getElementById(idContainer);
+function darkModeSwitch(){
 //a method of DOMTokenList for add a class or remove if present
 document.body.classList.toggle("darkMode");
+}
+
+function showCode() {
+    let btnToShowCode = document.getElementById("showMoreId");
+    let codeToShow = document.getElementById("codeContainerId")
+    if (btnToShowCode) {
+        btnToShowCode.classList.toggle("hide");
+        btnToShowCode.classList.toggle("flexDisplay");
+    } else {
+        console.error("L'élément bouton avec l'ID " + showMoreId + " n'existe pas.");
+    }
+    
+    if (codeToShow) {
+        codeToShow.classList.toggle("hide");
+        codeToShow.classList.toggle("flexDisplay");
+    } else {
+        console.error("L'élément conteneur de code avec l'ID " + codeContainerId + " n'existe pas.");
+    }
+    console.log("show code");
 }
 
 // document.querySelectorAll('.ulParent > li').forEach(function(menuItem) {
