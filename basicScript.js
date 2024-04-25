e44c9cb750138ec895491538edc46ae016d74a69
Moulay-E <ayoub.edda2@gmail.com>
2024-04-25 12:47:22 +0200

    adjust page name , order the js code

--- basicScript.js
+++ basicScript.js
@@ -3,27 +3,6 @@ function darkModeSwitch(){
 document.body.classList.toggle("darkMode");
 }
 
-function showCode(clickedBtn) {
-    let btnToShowCode = document.querySelector(".showMore")
-    const parent = clickedBtn.parentElement;
-    const codeToShow = parent.querySelector(".codeContainerJs");
-    if (btnToShowCode) {
-        btnToShowCode.classList.toggle("hide");
-        btnToShowCode.classList.toggle("flexDisplay");
-    } else {
-        console.error("L'élément bouton avec l'ID " + btnToShowCode + " n'existe pas.");
-    }
-    
-    if (codeToShow) {
-        codeToShow.classList.toggle("hide");
-        codeToShow.classList.toggle("flexDisplay");
-    } else {
-        console.error("L'élément conteneur de code avec l'ID " + codeToShow + " n'existe pas.");
-    }
-    console.log("show code");
-}
-
-
 //add eventListener on button to hide the btn and show the code, she he added to every btn, and use "this" to target the btn and the class who contain the code to show
 document.addEventListener('DOMContentLoaded', function ShowCodeAndHideHimself(){
     const showMoreBtn = document.querySelectorAll('.showMore');
---
+++ scripts/string.js
@@ -0,0 +1,6 @@
+function stringLength(param){
+    let text = textParam;
+    let textLength = text.textLength;
+    let textResult = document.querySelector(".textResult");
+    textResult.innerText = textLength;
+}
