2dd77e56123c58b67bb77765845c66a0013fcbe0
Moulay-E <ayoub.edda2@gmail.com>
2024-06-05 10:18:12 +0200

    add arr creation

--- scripts/arrays/array.js
+++ scripts/arrays/array.js
@@ -1,19 +1,16 @@
-function numberEpsilon() {
-    const data = document.getElementById("numberEpsilonValueId").value;
-    const showResult = document.getElementById("numberEpsilonIdResult");
-    let stringArr = data.split(" ");
-    let result = 0;
-    let dataInitial = "";
-
-    stringArr.forEach((e)=>{
-        // to prevent from adding other than a number as a stringArr
-        result = (isNaN(Number(e))) ? result: result + Math.abs(Number(e));
-        dataInitial += e + '|';
-    });
-    console.log(result);
-
-    let UnderEpsilon = (result - result)  < Number.EPSILON;
-    showResult.innerHTML = `Les nombres de base : ${dataInitial} . </br>
-     Le total : ${result}. </br>
-    Les erreur de calcul si il y en as sont elle plus petite que epsilon ? : ${UnderEpsilon} `
+function arrayCreate() {
+    const data = document.getElementById("arrayCreateValueId").value;
+    const showResult = document.getElementById("arrayCreateIdResult");
+    
+    let arr = data.split(" ");
+    showResult.innerHTML = `
+    le tableau crée avec vos valeur : [${arr}] </br> `
+}
+function arrayNew() {
+    const data = document.getElementById("arrayNewValueId").value;
+    const showResult = document.getElementById("arrayNewIdResult");
+    
+    let arr = data.split(" ");
+    showResult.innerHTML = `
+    le tableau crée avec vos valeur : [${arr}] </br> `
 }
