3424c577ada56c73de0161ab76875ad66df82875
Moulay-E <ayoub.edda2@gmail.com>
2024-06-05 07:50:01 +0200

    add number minvalue, and number safe interger

--- scripts/numbers/numberProperties.js
+++ scripts/numbers/numberProperties.js
@@ -17,3 +17,33 @@ function numberEpsilon() {
      Le total : ${result}. </br>
     Les erreur de calcul si il y en as sont elle plus petite que epsilon ? : ${UnderEpsilon} `
 }
+function numberMaxValue() {
+    const data = document.getElementById("numberMaxValueValueId").value;
+    const showResult = document.getElementById("numberMaxValueIdResult");
+    let maxNumber = Number.MAX_VALUE;
+    let result = (data * maxNumber);
+
+    showResult.innerHTML = `Le nombre donnés : ${data} . </br>
+     Le max number en js : ${maxNumber}. </br>
+    La multiplication des deux: ${result} `
+}
+function numberMinValue() {
+    const data = document.getElementById("numberMinValueValueId").value;
+    const showResult = document.getElementById("numberMinValueIdResult");
+    let minNumber = Number.MIN_VALUE;
+    let result = (minNumber - data);
+
+    showResult.innerHTML = `Le nombre donnés : ${data} . </br>
+     Le max number en js : ${minNumber}. </br>
+    La soustraction de minValue par le nombre: ${result} `
+}
+function numberMaxSafeInteger() {
+    const data = document.getElementById("numberMaxSafeIntegerValueId").value;
+    const showResult = document.getElementById("numberMaxSafeIntegerIdResult");
+    let maxSafeInterger = Number.MAX_SAFE_INTEGER;
+    let result = (maxSafeInterger * data);
+
+    showResult.innerHTML = `Le nombre donnés : ${data} . </br>
+     Le max number en js : ${maxSafeInterger}. </br>
+    La multiplication de safeMaxInteger par le nombre: ${result} `
+}
