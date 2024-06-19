8d4a2162766657d67d4e314dd452ac1b326df060
Moulay-E <ayoub.edda2@gmail.com>
2024-06-19 16:56:06 +0200

    add arr find methods

--- scripts/arrays/arraySearchMethods.js
+++ scripts/arrays/arraySearchMethods.js
@@ -20,3 +20,37 @@ function arrMethodslastIndexOf() {
     }
     showResult.innerHTML = `The index of the element is : ${result} . `
 }
+function arrMethodsIncludes() {
+    const data = document.getElementById("arrMethodsIncludesValueId").value;
+    const showResult = document.getElementById("arrMethodsIncludesIdResult");
+    let arr =  ["Apple", "Orange", "Apple", "Mango", "Orange"];
+    let result = "";
+
+    let search = arr.includes(data);
+    if(search){
+        result = "The element is present in the table";
+    }
+    else {
+        result = "The element is not present in the table";
+    }
+    showResult.innerHTML = ` ${result} . `
+}
+function arrMethodsFind() {
+    const data = document.getElementById("arrMethodsFindValueId").value;
+    const showResult = document.getElementById("arrMethodsFindIdResult");
+    let arr =  [4, 9, 16, 25, 29];
+    let result = "";
+    let search;
+    function moreThanData(value, index, array) {
+        return value > data;
+      }
+    // search = arr.find(data);
+    if(Number(data)){
+        search = arr.find(moreThanData);
+        result = "The element is present in the table";
+    }
+    else {
+        result = "The element is not present in the table";
+    }
+    showResult.innerHTML = ` ${result} . `
+}
