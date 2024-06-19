36a59d9d624777ddafae3de03d8816d186009835
Moulay-E <ayoub.edda2@gmail.com>
2024-06-19 18:47:25 +0200

    finish html and js for array search methods

--- scripts/arrays/arraySearchMethods.js
+++ scripts/arrays/arraySearchMethods.js
@@ -54,3 +54,56 @@ function arrMethodsFind() {
     }
     showResult.innerHTML = ` ${result}. `
 }
+function arrMethodsfindIndex() {
+    const data = document.getElementById("arrMethodsfindIndexValueId").value;
+    const showResult = document.getElementById("arrMethodsfindIndexIdResult");
+    let arr =  [4, 9, 16, 25, 29];
+    let result = "";
+    let search;
+    function moreThanData(value, index, array) {
+        return value > data;
+      }
+    if(Number(data)){
+        search = arr.findIndex(moreThanData);
+        result = `The number you have type : ${data}. </br>
+                  The index of number greater than that : ${search} `;
+    }
+    else {
+        result = "The element is not present in the table";
+    }
+    showResult.innerHTML = ` ${result}. `
+}
+function arrMethodsindLast() {
+    const data = document.getElementById("arrMethodsindLastValueId").value;
+    const showResult = document.getElementById("arrMethodsindLastIdResult");
+    let arr =  [27, 28, 30, 40, 42, 35, 30];
+    let result = "";
+    let search;
+   
+    if(Number(data)){
+        search = arr.findLast(x => x > data);
+        result = `The number you have type : ${data}. </br>
+                  The number greater than that from the end : ${search} `;
+    }
+    else {
+        result = "The element is not present in the table";
+    }
+    showResult.innerHTML = ` ${result}. `
+}
+function arrMethodsindLastIndex() {
+    const data = document.getElementById("arrMethodsindLastIndexValueId").value;
+    const showResult = document.getElementById("arrMethodsindLastIndexIdResult");
+    let arr =  [27, 28, 30, 40, 42, 35, 30];
+    let result = "";
+    let search;
+   
+    if(Number(data)){
+        search = arr.findLastIndex(x => x > data);
+        result = `The number you have type : ${data}. </br>
+                  The index of number greater than that from the end : ${search} `;
+    }
+    else {
+        result = "The element is not present in the table";
+    }
+    showResult.innerHTML = ` ${result}. `
+}
