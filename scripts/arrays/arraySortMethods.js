26f04dc125b2263d6b1613306770dbc6f8024498
Moulay-E <ayoub.edda2@gmail.com>
2024-06-21 14:15:58 +0200

    ajout de array reverse

--- scripts/arrays/arraySortMethods.js
+++ scripts/arrays/arraySortMethods.js
@@ -1,15 +1,32 @@
-function arrMethodsIncludes() {
-    const data = document.getElementById("arrMethodsIncludesValueId").value;
-    const showResult = document.getElementById("arrMethodsIncludesIdResult");
+function arrMethodsSort() {
+    const data = document.getElementById("arrMethodsSortValueId").value;
+    const showResult = document.getElementById("arrMethodsSortIdResult");
     let arr =  ["Apple", "Orange", "Apple", "Mango", "Orange"];
-    let result = "";
+    // let result = "";
+    arr.push(data);
 
-    let search = arr.includes(data);
-    if(search){
-        result = "The element is present in the table";
-    }
-    else {
-        result = "The element is not present in the table";
-    }
-    showResult.innerHTML = ` ${result} . `
+    let result = arr.sort();
+    // if(search){
+    //     result = "The element is present in the table";
+    // }
+    // else {
+    //     result = "The element is not present in the table";
+    // }
+    showResult.innerHTML = `The alphabetical sorting array : ${result} . `
+}
+function arrMethodsReverse() {
+    const data = document.getElementById("arrMethodsReverseValueId").value;
+    const showResult = document.getElementById("arrMethodsReverseIdResult");
+    let arr =  ["Apple", "Orange", "Apple", "Mango", "Orange"];
+    // let result = "";
+    arr.push(data);
+
+    let result = arr.sort();
+    // if(search){
+    //     result = "The element is present in the table";
+    // }
+    // else {
+    //     result = "The element is not present in the table";
+    // }
+    showResult.innerHTML = `The alphabetical sorting array : ${result} . `
 }
