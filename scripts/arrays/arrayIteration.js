98475c5a5f793b183525ba5e787fe88b8a2e0e23
Moulay-E <ayoub.edda2@gmail.com>
2024-06-26 11:07:39 +0200

    add forEach and map methods

--- scripts/arrays/arrayIteration.js
+++ scripts/arrays/arrayIteration.js
@@ -1,17 +1,29 @@
-function arrMethodsindLastIndex() {
-    const data = document.getElementById("arrMethodsindLastIndexValueId").value;
-    const showResult = document.getElementById("arrMethodsindLastIndexIdResult");
+function arrForEach() {
+    const data = document.getElementById("arrForEachValueId").value;
+    const showResult = document.getElementById("arrForEachIdResult");
     let arr =  [27, 28, 30, 40, 42, 35, 30];
     let result = "";
-    let search;
+
+    arr.forEach( (element,index)=> {
+        result += ` The index ${index} is : ${element} </br> `;
+    })
+    showResult.innerHTML = `</br> ${result}. `
+}
+function arrMap() {
+    const data = document.getElementById("arrMapValueId").value;
+    const showResult = document.getElementById("arrMapIdResult");
+    let arr =  [45, 4, 9, 16, 25];
+    
+    const multiply = (e) => e * 2;
+    let result = arr.map(multiply);
    
-    if(Number(data)){
-        search = arr.findLastIndex(x => x > data);
-        result = `The number you have type : ${data}. </br>
-                  The index of number greater than that from the end : ${search} `;
-    }
-    else {
-        result = "The element is not present in the table";
-    }
-    showResult.innerHTML = ` ${result}. `
+    // if(Number(data)){
+    //     search = arr.findLastIndex(x => x > data);
+    //     result = `The number you have type : ${data}. </br>
+    //               The index of number greater than that from the end : ${search} `;
+    // }
+    // else {
+    //     result = "The element is not present in the table";
+    // }
+    showResult.innerHTML = ` The array element multiply by 2 </br> ${result}. `
 }
