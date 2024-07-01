0c593a0504d1fd6dc9c079cc6932288633ca8145
Moulay-E <ayoub.edda2@gmail.com>
2024-07-01 15:59:04 +0200

    add new date , and date(string)

--- scripts/date/date.js
+++ scripts/date/date.js
@@ -1,17 +1,24 @@
-function arrSome() {
-    const data = document.getElementById("arrSomeValueId").value;
-    const showResult = document.getElementById("arrSomeIdResult");
+function dateNewDate() {
+    const data = document.getElementById("dateNewDateValueId").value;
+    const showResult = document.getElementById("dateNewDateIdResult");
+    let result = new Date() ;
+
+    showResult.innerHTML = `The date now:  ${result}. `
+}
+function dateNewDateString() {
+    const data = document.getElementById("dateNewDateStringValueId").value;
+    const showResult = document.getElementById("dateNewDateStringIdResult");
     let arr =  [45, 4, 9, 16, 25];
-    let result ;
-    const addAllElement = (element, index, arr) => element < data ;
-    result = arr.reduceRight(addAllElement);
-    if(Number(data)) {
-        result = arr.some(addAllElement);
-    }
-    else {
-        result = 'Type a number please.';
-    }
-    showResult.innerHTML = ` Your value: ${data} . </br>
-    The array: ${arr} .</br>
-    </br> Is the entered value greater than A values in the array?  ${result}. `
+    let dateString = "September 5, 2016"
+    let result = new Date(dateString) ;
+    // const addAllElement = (element, index, arr) => element < data ;
+    // result = arr.reduceRight(addAllElement);
+    // if(Number(data)) {
+    //     result = arr.some(addAllElement);
+    // }
+    // else {
+    //     result = 'Type a number please.';
+    // }
+    showResult.innerHTML = ` The date string: ${dateString} . </br>
+    The date showed:  ${result}. `
 }
