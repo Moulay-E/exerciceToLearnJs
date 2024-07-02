690c081f064b8740c3d238cdc74853e1571e6e0f
Moulay-E <ayoub.edda2@gmail.com>
2024-07-02 17:01:35 +0200

    add newDateNumber to date

--- scripts/date/date.js
+++ scripts/date/date.js
@@ -8,9 +8,18 @@ function dateNewDate() {
 function dateNewDateString() {
     const data = document.getElementById("dateNewDateStringValueId").value;
     const showResult = document.getElementById("dateNewDateStringIdResult");
-    let arr =  [45, 4, 9, 16, 25];
     let dateString = "September 5, 2016"
     let result = new Date(dateString) ;
+
+    showResult.innerHTML = ` The date string: ${dateString} . </br>
+    The date showed:  ${result}. `
+}
+function dateNewDateNumber() {
+    const data = document.getElementById("dateNewDateNumberValueId").value;
+    const showResult = document.getElementById("dateNewDateNumberIdResult");
+    let arr =  [45, 4, 9, 16, 25];
+    let dateString = "September 5, 2016"
+    let result = new Date(2016, 11, 24, 10, 33, 30, 0) ;
     // const addAllElement = (element, index, arr) => element < data ;
     // result = arr.reduceRight(addAllElement);
     // if(Number(data)) {
@@ -19,6 +28,6 @@ function dateNewDateString() {
     // else {
     //     result = 'Type a number please.';
     // }
-    showResult.innerHTML = ` The date string: ${dateString} . </br>
+    showResult.innerHTML = ` The date string: 2016, 11, 24, 10, 33, 30, 0 . </br>
     The date showed:  ${result}. `
 }
