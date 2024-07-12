cadd96ed6e9061618d92d3be3f2ed4a0e57a60c8
Moulay-E <ayoub.edda2@gmail.com>
2024-07-12 17:40:24 +0200

    add & import navBar component to index.html , adjust css navbar

--- scripts/components/navBar.js
+++ scripts/components/navBar.js
@@ -4,16 +4,14 @@ function navBar(){
     let nav = document.getElementById("nav");
     let htmlToShow = `
     <div class="navigation__button">
-    <div class="flexCenter">
-        <a href="/index.html" > 
+        <a class="flexCenter" href="/index.html" > 
             <img class="navigation__home__img" src="/assets/home-pink-svg.svg" alt="home"/>
         </a> 
-    </div>
     <div class="navigation__darkmode" id="darkModeBtn" onclick="darkModeSwitch()">
         <img class="navigation__darkmode__img" src="/assets/night-pink-mode-svg.svg" alt="dark-mode" />
     </div>
 
-</div>
+    </div>
 
 <div class="menu">
     <div class="menu__btnContainer flexCenter">
