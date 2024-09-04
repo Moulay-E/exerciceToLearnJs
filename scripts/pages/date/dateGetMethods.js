37483aa5479f741a939da352f5736762783efbba
Moulay-E <ayoub.edda2@gmail.com>
2024-09-04 13:02:49 +0200

    add 3 element to the date methods pages

--- scripts/pages/date/dateGetMethods.js
+++ scripts/pages/date/dateGetMethods.js
@@ -1,7 +1,35 @@
-function isoDateHourMinuteSecond() {
-    const data = document.getElementById("isoDateHourMinuteSecondValueId").value;
-    const showResult = document.getElementById("isoDateHourMinuteSecondIdResult");
-    let result = new Date("2015-03-25T12:00:00Z") ;
+
+function newDateConstructor() {
+    const data = document.getElementById("newDateConstructorValueId").value;
+    const showResult = document.getElementById("newDateConstructorIdResult");
+    let result = new Date(data) ;
 
     showResult.innerHTML = `The date now:  ${result}. `
+}
+function getFullYear() {
+    const data = document.getElementById("getFullYearValueId").value;
+    const showResult = document.getElementById("getFullYearIdResult");
+    let date = new Date("2021-03-25") ;
+    let year = date.getFullYear();
+
+    showResult.innerHTML = `The date:  ${date}. <br>
+        The year: ${year} `
+}
+function getMonth() {
+    const data = document.getElementById("getMonthValueId").value;
+    const showResult = document.getElementById("getMonthIdResult");
+    let date = new Date("2021-03-25") ;
+    let month = date.getMonth();
+
+    showResult.innerHTML = `The date:  ${date}. <br>
+        The month: ${month+1} `
+}
+function getDate() {
+    const data = document.getElementById("getDateValueId").value;
+    const showResult = document.getElementById("getDateIdResult");
+    let date = new Date("2021-03-25") ;
+    let day = date.getDate();
+
+    showResult.innerHTML = `The date:  ${date}. <br>
+        The day: ${day} `
 }
