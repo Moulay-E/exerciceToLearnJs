58630a8cc927e2c06c4459aed8f8117708d25543
Moulay-E <ayoub.edda2@gmail.com>
2024-09-07 12:07:49 +0200

    add date get methods

--- scripts/pages/date/dateGetMethods.js
+++ scripts/pages/date/dateGetMethods.js
@@ -9,7 +9,7 @@ function newDateConstructor() {
 function getFullYear() {
     const data = document.getElementById("getFullYearValueId").value;
     const showResult = document.getElementById("getFullYearIdResult");
-    let date = new Date("2021-03-25") ;
+    let date = new Date() ;
     let year = date.getFullYear();
 
     showResult.innerHTML = `The date:  ${date}. <br>
@@ -18,18 +18,56 @@ function getFullYear() {
 function getMonth() {
     const data = document.getElementById("getMonthValueId").value;
     const showResult = document.getElementById("getMonthIdResult");
-    let date = new Date("2021-03-25") ;
+    let date = new Date() ;
     let month = date.getMonth();
 
     showResult.innerHTML = `The date:  ${date}. <br>
         The month: ${month+1} `
 }
-function getDate() {
-    const data = document.getElementById("getDateValueId").value;
-    const showResult = document.getElementById("getDateIdResult");
-    let date = new Date("2021-03-25") ;
+function dateGetDate() {
+    const data = document.getElementById("dateGetDateValueId").value;
+    const showResult = document.getElementById("dateGetDateIdResult");
+    let date = new Date() ;
     let day = date.getDate();
 
     showResult.innerHTML = `The date:  ${date}. <br>
         The day: ${day} `
+}
+function dateGetHour() {
+    const data = document.getElementById("dateGetHourValueId").value;
+    const showResult = document.getElementById("dateGetHourIdResult");
+    let date = new Date() ;
+    let hour = date.getHours();
+
+    showResult.innerHTML = `The date:  ${date}. <br>
+        The hour: ${hour} `
+}
+function dateGetseconds() {
+    const data = document.getElementById("dateGetsecondsValueId").value;
+    const showResult = document.getElementById("dateGetsecondsIdResult");
+    let date = new Date() ;
+    let seconds = date.getSeconds();
+
+    showResult.innerHTML = `The date:  ${date}. <br>
+        The seconds: ${seconds} `
+}
+function dateGetMilliseconds() {
+    const data = document.getElementById("dateGetMillisecondsValueId").value;
+    const showResult = document.getElementById("dateGetMillisecondsIdResult");
+    let date = new Date() ;
+    let milliseconds = date.getMilliseconds();
+
+    showResult.innerHTML = `The date:  ${date}. <br>
+        The milliseconds: ${milliseconds} `
+}
+function dateGetday() {
+    const data = document.getElementById("dateGetdayValueId").value;
+    const showResult = document.getElementById("dateGetdayIdResult");
+    let date = new Date() ;
+    let day = date.getDay();
+    let dayName = ["Sunday","Monday ","Tuesday ","Wednesday ","Thursday ","Friday","Saturday "];
+
+    showResult.innerHTML = `The date:  ${date}. <br>
+        The return value ${day} <br>
+        The day: ${dayName[day]} `
 }
