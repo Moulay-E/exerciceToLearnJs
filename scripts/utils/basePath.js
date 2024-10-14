1e2494b7ecfc6d1387858bbf34d3c31de0844411
Moulay-E <ayoub.edda2@gmail.com>
2024-10-14 15:41:16 +0200

    correct the link

---
+++ scripts/utils/basePath.js
@@ -0,0 +1,3 @@
+// use the window object to check the url and adapt it, to not change anything between local url and published url
+
+export const basePath = window.location.pathname.includes('your-project-name') ? '/your-project-name/' : '/';
