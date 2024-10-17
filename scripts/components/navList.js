0396fb2ba64b77fec4061f56df2fc29fcebe4d76
Moulay-E <ayoub.edda2@gmail.com>
2024-10-17 11:21:35 +0200

    maj path of link

--- scripts/components/navList.js
+++ scripts/components/navList.js
@@ -1,4 +1,5 @@
 import { fetchData } from "./../fetch/fetchData.js";
+import { basePath } from "../../config.js";
 export async function navList(){
     const url = "data/dataNavLink.json" ;
     const data = await fetchData(url);
@@ -6,7 +7,7 @@ export async function navList(){
     const navArea = document.getElementById("navList");
     let result = "<h2>Menu Bar</h2>";
     data.navLink.forEach((e)=> {
-        let html = ` <a class="menu__container__bar__list__element" href=${e.link}> ${e.title} </a>`
+        let html = ` <a class="menu__container__bar__list__element" href=${basePath + e.link}> ${ e.title} </a>`
         result+= html;
     })
     navArea.innerHTML = result;
